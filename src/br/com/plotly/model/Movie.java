d1e1cd318907a4d42b18492a0ef1e589a34bbdc5
officialdelffin <workdelffin@gmail.com>
2026-03-09 12:51:51 -0300

    commit 4

--- src/br/com/plotly/model/Movie.java
+++ src/br/com/plotly/model/Movie.java
@@ -6,7 +6,22 @@ package br.com.plotly.model;
 public class Movie {
 
 
+    // Construtores :
 
+    public Movie (String title , int year, int released, int runtime, String genre, String write, String plot) {}
+
+
+    // Atributos :
+    private String title;
+    private int year;
+    private int released;
+    private int runtime;
+    private String genre;
+    private String write;
+    private String plot;
+
+
+    // Métodos :
 
 
 }
