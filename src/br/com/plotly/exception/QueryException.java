f0593b3b8d509c6c4408615c57fa81d3ff3086e1
officialdelffin <workdelffin@gmail.com>
2026-04-08 16:31:59 -0300

    commit 81

--- src/br/com/plotly/exception/QueryException.java
+++ src/br/com/plotly/exception/QueryException.java
@@ -67,6 +67,10 @@ public class QueryException {
                 """, errorConection.getMessage());
 
 
+        // Aviso para tentar novamente :
+        System.out.println("Clique na tecla ENTER para fazer uma nova consulta: ");
+
+
         // Executando novamente a função de escolher um titulo :
         newInitiation.interactionUser();
 
